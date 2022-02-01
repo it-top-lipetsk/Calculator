3174fe9bca3bb34a19dfaef1780c8c2193c65700
Starinin Andrey <starininandrey@gmail.com>
2022-02-01 21:10:22 +0300

    Init

---
+++ Calculator.ConsoleTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Calculator.Lib;
+
+namespace Calculator.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = 5;
+            var y = 10;
+            var y_error = 0;
+
+            var add = Calc.Add(x, y);
+            Console.WriteLine($"{x} + {y} = {add}");
+
+            var div = Calc.Div(x, y);
+            Console.WriteLine($"{x} / {y} = {div}");
+
+            try
+            {
+                var div_error = Calc.Div(x, y_error);
+                Console.WriteLine($"{x} / {y_error} = {div}");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Делить на ноль нельзя!");
+            }
+        }
+    }
+}
---
+++ Calculator.Test/TestDiv.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Calculator.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class TestDiv
+    {
+        [TestMethod]
+        public void Div_10_2_5()
+        {
+            var x = 10;
+            var y = 2;
+
+            var actual = Calc.Div(x, y);
+            var expected = 5;
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Div_5_2_25()
+        {
+            var x = 10;
+            var y = 2;
+
+            var actual = Calc.Div(x, y);
+            var expected = 5;
+            
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Div_Exception()
+        {
+            var x = 10;
+            var y = 0;
+
+            Calc.Div(x, y);
+        }
+    }
+}
